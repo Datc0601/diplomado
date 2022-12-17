a7aac26b656a1f484c6bec79b8b69a3d9a1c217d
Datc0601 <99293417+Datc0601@users.noreply.github.com>
2022-12-17 03:48:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from sklearn import svm
 
 
 
-@st.cache(allow_output_mutation=True, ttl= 5)
+@st.cache(allow_output_mutation=True, ttl= 0.01)
 def cargar_de_datos():
     leer_columnas = ['ESTU_DEPTO_RESIDE','ESTU_GENERO','FAMI_TIENECOMPUTADOR','FAMI_TIENEINTERNET','FAMI_ESTRATOVIVIENDA','FAMI_COMECARNEPESCADOHUEVO','PUNT_GLOBAL']
     df = pd.read_csv("icfes_data.csv",low_memory=False, usecols=leer_columnas)
